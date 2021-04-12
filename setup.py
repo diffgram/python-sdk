2e9651ab10c9e6c81d36442e9ae5923b4fa1c432
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-04-12 11:51:03 -0700

    init

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import diffgram.__init__ as init
+
+with open(".././README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = init.__name__,
+    version = init.__version__,
+    author="Diffgram",
+    author_email="support@diffgram.com",
+    description="SDK for Diffgram",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diffgram/python-sdk",
+    packages=setuptools.find_packages(
+		exclude=["test", "samples", "ops_scripts"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+	   'requests>=2.20.1',
+	   'opencv-python>=4.0.0.21',
+	   'scipy>=1.1.0',
+	   'six>=1.9.0',
+	   'pillow>=6.1.0'
+	]
+)
