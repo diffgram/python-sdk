ab15e8ccf7cb7ecb361d15b13a15e43d850bf3c5
Pablo <pjestradac@gmail.com>
2021-09-28 11:47:03 -0600

    wip: sensor fusion JSON upload

--- sdk/diffgram/file/file_3d.py
+++ sdk/diffgram/file/file_3d.py
@@ -6,6 +6,7 @@ import io
 import math
 import uuid
 
+
 class File3D:
     point_list: list
     client: Project
@@ -22,26 +23,23 @@ class File3D:
         })
         return self.point_list
 
-    def upload(self, dataset_name = None):
+    def upload(self, dataset_name = None, chunk_size = 5000000):
         """
             Builds a JSON file from current point list and uploads it to
             Diffgram.
+        :param dataset_name:
+        :param chunk_size: Size of each chunk of the JSON file. Default is 5MB
         :return:
         """
         file_data = {
             'point_list': self.point_list
         }
-        headers = {
-            'content-type': 'multipart/form-data',
-
-        }
         json_data = json.dumps(file_data)
 
-        endpoint = "/api/walrus/v1/project/{}/upload/large".format(
+        endpoint = "/api/walrus/project/{}/upload/large".format(
             self.client.project_string_id
         )
-        # chunk_size = 5000000  # 5 MB chunks
-        chunk_size = 1000000  # 1 MB chunks
+        chunk_size = 5000000  # 5 MB chunks
         dataset_id = self.client.default_directory['id']
         if dataset_name is not None:
             dataset_id = self.client.directory.get(dataset_name).id
@@ -49,36 +47,43 @@ class File3D:
         with io.StringIO(json_data) as s:
             pos = s.tell()
             s.seek(0, os.SEEK_END)
-            file_size =  s.tell()
-            print('FILE SIZE', file_size)
+            file_size = s.tell()
             s.seek(pos)
-
-            # requests.post('http://some.url/streamed', data = f)
             num_chunks = int(math.ceil(file_size / chunk_size))
             last_chunk_size = -1
             if file_size % chunk_size != 0:
                 last_chunk_size = file_size % chunk_size
 
-            print('Num chunks', num_chunks)
-            print('chunk_size', chunk_size)
-            print('last_chunk_size', last_chunk_size)
             uid_upload = str(uuid.uuid4())
             for i in range(0, num_chunks):
                 # print('File Size: {} bytes.'.format(file_size))
+
                 payload = {
-                    'dzuid': uid_upload,
+                    'dzuuid': uid_upload,
                     'dzchunkindex': i,
                     'dztotalfilesize': file_size,
                     'dzchunksize': chunk_size,
                     'dztotalchunkcount': num_chunks,
                     'dzchunkbyteoffset': i * chunk_size,
                     'directory_id': dataset_id,
-                    'source': 'api',
-                    'file': 'binary data here',
+                    'source': 'from_sensor_fusion_json',
                 }
-
+                # Adjust final chunk size
                 if i == (num_chunks - 1) and last_chunk_size != -1:
                     # Read last chunk size here...
                     payload['dzchunksize'] = last_chunk_size
-                print(payload)
-            return file_size
+
+                # Read file Chunk
+                s.seek(payload['dzchunkbyteoffset'])
+                file_chunk = s.read(payload['dzchunksize'])
+                files = {'file': ('{}_sensor_fusion_file.json'.format(uid_upload), file_chunk)}
+                # Make request to server
+                url = self.client.host + endpoint
+                response = self.client.session.post(url,
+                                                    data = payload,
+                                                    files = files)
+
+                self.client.handle_errors(response)
+                # print(payload)
+
+            return True
