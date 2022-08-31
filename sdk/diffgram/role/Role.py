f51af9bfadcbea072afb991cd717b48ea4fb21e5
Pablo <pjestradac@gmail.com>
2022-08-31 15:19:32 -0600

    wip: roles

--- sdk/diffgram/role/Role.py
+++ sdk/diffgram/role/Role.py
@@ -93,6 +93,58 @@ class Role:
         self.permissions_list = role.permissions_list
         return role
 
+    def remove_permission(self, perm: str, object_type: str):
+        endpoint = f"/api/v1/project/{self.client.project_string_id}/roles/{self.id}/remove-perm"
+
+        response = self.client.session.patch(
+            self.client.host + endpoint, json = {
+                'permission': perm,
+                'object_type': object_type,
+            })
+
+        self.client.handle_errors(response)
+
+        data = response.json()
+        role = Role(client = self.client, role_dict = data)
+        self.permissions_list = role.permissions_list
+        return role
+
+    def assign_to_member_in_object(self, member_id: int, object_id: int, object_type: str):
+        endpoint = f"/api/v1/project/{self.client.project_string_id}/role-member-object"
+
+        if not self.id:
+            raise Exception(f'Provide role ID')
+
+        response = self.client.session.patch(
+            self.client.host + endpoint, json = {
+                'member_id': member_id,
+                'role_id': self.id,
+                'object_type': object_type,
+                'object_id': object_id,
+            }
+        )
+
+        self.client.handle_errors(response)
+        role_member_object = response.json()
+        return role_member_object
+
+    def get(self, name: str) -> list:
+
+        endpoint = f"/api/v1/project/{self.client.project_string_id}/roles"
+
+        response = self.client.session.get(self.client.host + endpoint)
+
+        self.client.handle_errors(response)
+        roles = response.json()
+        result = None
+        for r in roles:
+            if r.get('name') == name:
+                result = r
+                break
+
+        role_obj = Role(client = self.client, role_dict = result)
+        return role_obj
+
     def list(self) -> list:
 
         endpoint = f"/api/v1/project/{self.client.project_string_id}/roles"
