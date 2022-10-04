67922d3c19b749639458cf0c056cd174a99d1ba1
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-10-04 16:28:09 -0300

    Add typing for the model output

--- sdk/samples/model_server.py
+++ sdk/samples/model_server.py
@@ -1,11 +1,11 @@
-from diffgram import DiffgramBaseModel, Instance
+from diffgram import DiffgramBaseModel, Prediction, Instance, Attribute
 from fastapi import FastAPI
-
 class MyTestModel(DiffgramBaseModel):
     def infere(self, file):
-        return [
-            Instance(id=1)
-        ]
+        return Prediction(
+            instances=[Instance(id=1)],
+            attributes=[Attribute(id=1)]
+        )
 
     def get_schema(self):
         return super().get_schema()
